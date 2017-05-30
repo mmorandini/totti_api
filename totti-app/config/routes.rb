ebb1d7e7e81df677c1be669e32208b565e4dbcf3
HKeating <horacekeating@hotmail.co.uk>
2017-05-30 14:51:44 +0100

    register and login functions working

--- totti-app/config/routes.rb
+++ totti-app/config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
-  resources :users
+
+  scope :api do
+    resources :posts
+    resources :users
+    post 'register', to: 'authentications#register'
+    post 'login', to: 'authentications#login'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
